3dc9dffa3c2830537fb777b70affa5bb8b18dd7c
Fantox <shubhapratimbiswas@gmail.com>
2022-08-23 16:35:41 +0530

    Update haikal.js

--- haikal.js
+++ haikal.js
@@ -2635,7 +2635,7 @@ break
 case 'music': case 'play': case 'song': case 'ytplay': {
  let yts = require("yt-search")
  let search = await yts(text)
- let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
+ let anu = search.videos[0]
  let ytvc = await hx.youtube(anu.url)
  let buttons = [
  {buttonId: `${prefix}ytvd ${ytvc.link}`, buttonText: {displayText: '► Video'}, type: 1},
